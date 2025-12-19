e7973a5a60f63b736f2d9acc37504cffc71c366a
Mikita Hashkin <n.gashkin@gmail.com>
2025-12-19 16:08:48 +0200

    Update application-test.yml and test-data.sql for test environment configuration

--- user-service/src/test/resources/test-data.sql
+++ user-service/src/test/resources/test-data.sql
@@ -1,3 +1,7 @@
+DELETE FROM user_roles;
+DELETE FROM users;
+DELETE FROM roles;
+
 INSERT INTO roles (id, name, created_at) VALUES (1, 'ROLE_USER', CURRENT_TIMESTAMP);
 INSERT INTO roles (id, name, created_at) VALUES (2, 'ROLE_PREMIUM_USER', CURRENT_TIMESTAMP);
 INSERT INTO roles (id, name, created_at) VALUES (3, 'ROLE_ADMIN', CURRENT_TIMESTAMP);
