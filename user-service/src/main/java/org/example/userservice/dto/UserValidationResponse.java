b1062491310257fc671618f3b92a3019c7b8aa4d
Mikita Hashkin <n.gashkin@gmail.com>
2026-02-04 20:29:16 +0200

    Initial commit

--- user-service/src/main/java/org/example/userservice/dto/UserValidationResponse.java
+++
@@ -1,9 +0,0 @@
-package org.example.userservice.dto;
-
-import java.util.List;
-
-public record UserValidationResponse(
-        boolean valid,
-        String username,
-        List<String> roles
-) {}
