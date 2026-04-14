86719bed516d7380cd73058dd1e57af41b1a895c
Mikita Hashkin <n.gashkin@gmail.com>
2026-04-14 20:34:34 +0300

    fix: register PeriodValidator reflection hint for analytics native image

--- analytics-service/src/main/java/org/example/analyticsservice/config/NativeImageConfig.java
+++ analytics-service/src/main/java/org/example/analyticsservice/config/NativeImageConfig.java
@@ -2,6 +2,7 @@ package org.example.analyticsservice.config;
 
 import com.example.cerps.common.dto.RateHistoryResponse;
 import com.example.cerps.common.dto.RatePoint;
+import com.example.cerps.common.validation.PeriodValidator;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
@@ -16,5 +17,6 @@ public class NativeImageConfig implements RuntimeHintsRegistrar {
     public void registerHints(final RuntimeHints hints, final ClassLoader classLoader) {
         hints.reflection().registerType(RateHistoryResponse.class, MemberCategory.values());
         hints.reflection().registerType(RatePoint.class, MemberCategory.values());
+        hints.reflection().registerType(PeriodValidator.class, MemberCategory.values());
     }
 }
